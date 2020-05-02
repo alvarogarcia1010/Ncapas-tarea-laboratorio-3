05409870599f36358720e3eb79a2cba906bfe97a
alvarogarcia1010 <00111317@uca.edu.sv>
2020-05-02 14:36:53 -0600

    Spring setup

---
+++ src/test/java/com/uca/capas/tarealabo3/TareaLabo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas.tarealabo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaLabo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
